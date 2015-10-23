b695d5cd9577e786531a1838371c09091a569fa8
Alejandra Garcia-Rojas <alegrm@gmail.com>
2015-10-23 14:02:29 +0200

    use jdeb

--- deb-package/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(triplegeo-service):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="triplegeo-service" command="/usr/bin/triplegeo-service"
--- deb-package/debian/triplegeo-service.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the triplegeo-service package
-#
-0 4	* * *	root	[ -x /usr/bin/triplegeo-service_maintenance ] && /usr/bin/triplegeo-service-ui_maintenance
