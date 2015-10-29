703f36af587d5355d8cc8f20987f0fa95743f514
Alejandra Garcia-Rojas <alegrm@gmail.com>
2015-10-29 12:00:20 +0100

    change upload to streaming

--- src/main/java/org/linkeddata/triplegeo/rest/FileUpload.java
+++ src/main/java/org/linkeddata/triplegeo/rest/FileUpload.java
@@ -1,8 +1,10 @@
 package org.linkeddata.triplegeo.rest;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.ServletContext;
@@ -15,11 +17,14 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileItemIterator;
+import org.apache.commons.fileupload.FileItemStream;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.fileupload.util.Streams;
+import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 
 import com.google.gson.Gson;
@@ -55,9 +60,11 @@ public class FileUpload {
   @Produces(MediaType.APPLICATION_JSON)
   public Response uploadFiles(@Context HttpServletRequest request, @Context ServletContext context) {
 
-    String candidateName = null;
+    // String candidateName = null;
     File dir = getWorkingDir(context);
     List<String> uploaded = new ArrayList<String>();
+
+    log.debug(request.getContentType());
 
     // checks whether there is a file upload request or not
     if (ServletFileUpload.isMultipartContent(request)) {
@@ -67,30 +74,48 @@ public class FileUpload {
         /*
          * parseRequest returns a list of FileItem but in old (pre-java5) style
          */
-        final List items = fileUpload.parseRequest(request);
+        // final List items = fileUpload.parseRequest(request);
 
-        if (items != null) {
-          final Iterator iter = items.iterator();
-          while (iter.hasNext()) {
-            final FileItem item = (FileItem) iter.next();
-            final String itemName = item.getName();
-            final String fieldName = item.getFieldName();
-            final String fieldValue = item.getString();
+        FileItemIterator iter = fileUpload.getItemIterator(request);
 
-            if (item.isFormField()) {
-              candidateName = fieldValue;
-              System.out.println("Field Name: " + fieldName + ", Field Value: " + fieldValue);
-              System.out.println("Candidate Name: " + candidateName);
-            } else {
-              final File savedFile =
-                  new File(dir.getPath() + File.separator + itemName.toLowerCase());
-              System.out.println("Saving the file: " + savedFile.getName());
-              item.write(savedFile);
-              uploaded.add(dir.getPath() + File.separator + itemName.toLowerCase());
+        // if (items != null) {
+        // final Iterator iter = items.iterator();
+        while (iter.hasNext()) {
+          final FileItemStream item = iter.next();
+          final String itemName = item.getName();
+          final String fieldName = item.getFieldName();
+          // final String fieldValue = item.getString();
+
+          InputStream stream = item.openStream();
+
+          if (item.isFormField()) {
+            // candidateName = stream.asString(stream);
+            System.out.println("Field Name: " + fieldName + ", andidate Name: "
+                + Streams.asString(stream));
+          } else {
+            System.out.println("File field " + fieldName + " with file name " + item.getName()
+                + " detected.");
+
+            final File targetFile =
+                new File(dir.getPath() + File.separator + itemName.toLowerCase());
+            // System.out.println("Saving the file: " + savedFile.getName());
+            // item.write(savedFile);
+
+            OutputStream outStream = new FileOutputStream(targetFile);
+
+            byte[] buffer = new byte[8 * 1024];
+            int bytesRead;
+            while ((bytesRead = stream.read(buffer)) != -1) {
+              outStream.write(buffer, 0, bytesRead);
             }
+            IOUtils.closeQuietly(stream);
+            IOUtils.closeQuietly(outStream);
 
+            uploaded.add(dir.getPath() + File.separator + itemName.toLowerCase());
           }
+
         }
+        // }
       } catch (FileUploadException e) {
         log.error(e);
         e.printStackTrace();
